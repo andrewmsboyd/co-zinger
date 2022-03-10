647b00540ad0d1aa371ee4458211ba1aaa6f89d3
Andy Boyd <amsb@pm.me>
2022-03-10 14:42:55 -0600

    rearranging and cleanup

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,18 @@ var port = 3000;
 module.exports = router;
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+/* router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+}); */
+
+
+
+// setup the listener for ticket note activity
+router.post('/note', (req, res) => {
+  console.log('got a message from zendesk');
+  res.send('zendesk target triggered!')
 });
 
 app.listen(port, () => {
   console.log(`Test app listening on port ${port} at /test context`);
-});
-
-app.post('/test', (req, res) => {
-  res.send('POST method received')
-});
-
-// setup the listener for ticket note activity
-router.get('/note', (req, res) => {
-  console.log('got a message from zendesk');
-  res.send('zendesk target triggered!')
 });
